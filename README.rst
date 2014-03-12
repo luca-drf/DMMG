f5d1e8e7020e493229db90b5929f58dcfe01f674
Luca Da Rin Fioretto <radome@gmail.com>
2014-03-12 01:18:35 -0600

    Initial commit

---
+++ README.rst
@@ -0,0 +1,24 @@
+Declarative Mathematical Model Generator (DMMG)
+===============================================
+DMMG project's goal is to provide a system to automatically generate
+mathematical models based on Natural Language Descriptions (NLD).
+
+Copyright (C) 2014 DMMG Project
+
+For license information, see LICENSE.txt
+
+
+Requirements
+------------
+- Python 2.7
+- nltk package http://nltk.org/
+
+
+Contributors
+------------
+Luca Da Rin Fioretto, Federico Campeotto
+
+.. How to install
+.. ==============
+
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name = "DMMG",
+    version = "0",
+    author = "Luca Da Rin Fioretto, Federico Campeotto",
+    author_email = "Luca Da Rin Fioretto <lucadrf@nmsu.edu>,\
+                    Federico Campeotto <campe8@nmsu.edu>",
+    description = (""),
+    license = "Apache License v2",
+    keywords = "",
+    url = "",
+    packages = ['dmmg', 'dmmg/tests'],
+    long_description = read('README.rst'),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: NLP",
+        "License :: Apache License v2",
+    ],
+)
